a056618ac36407d53546b132d511490c3741b771
harshsinghi <harsh.kumar.singhi1990@gmail.com>
2014-09-23 23:31:16 +0530

    Create Login.Desinger.cs

---
+++ Login.Desinger.cs
@@ -0,0 +1,213 @@
+namespace automobile
+{
+    partial class login1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rduser = new System.Windows.Forms.RadioButton();
+            this.rdadmin = new System.Windows.Forms.RadioButton();
+            this.rdmanager = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtuser = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Login = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtpassword = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox2.Controls.Add(this.rduser);
+            this.groupBox2.Controls.Add(this.rdadmin);
+            this.groupBox2.Controls.Add(this.rdmanager);
+            this.groupBox2.Location = new System.Drawing.Point(60, 148);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(336, 62);
+            this.groupBox2.TabIndex = 22;
+            this.groupBox2.TabStop = false;
+            // 
+            // rduser
+            // 
+            this.rduser.AutoSize = true;
+            this.rduser.BackColor = System.Drawing.Color.Black;
+            this.rduser.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rduser.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.rduser.Location = new System.Drawing.Point(235, 28);
+            this.rduser.Name = "rduser";
+            this.rduser.Size = new System.Drawing.Size(72, 22);
+            this.rduser.TabIndex = 5;
+            this.rduser.TabStop = true;
+            this.rduser.Text = "STAFF";
+            this.rduser.UseVisualStyleBackColor = false;
+            // 
+            // rdadmin
+            // 
+            this.rdadmin.AutoSize = true;
+            this.rdadmin.BackColor = System.Drawing.Color.Black;
+            this.rdadmin.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdadmin.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.rdadmin.Location = new System.Drawing.Point(17, 28);
+            this.rdadmin.Name = "rdadmin";
+            this.rdadmin.Size = new System.Drawing.Size(73, 22);
+            this.rdadmin.TabIndex = 3;
+            this.rdadmin.TabStop = true;
+            this.rdadmin.Text = "ADMIN";
+            this.rdadmin.UseVisualStyleBackColor = false;
+            // 
+            // rdmanager
+            // 
+            this.rdmanager.AutoSize = true;
+            this.rdmanager.BackColor = System.Drawing.Color.Black;
+            this.rdmanager.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdmanager.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.rdmanager.Location = new System.Drawing.Point(112, 28);
+            this.rdmanager.Name = "rdmanager";
+            this.rdmanager.Size = new System.Drawing.Size(101, 22);
+            this.rdmanager.TabIndex = 4;
+            this.rdmanager.TabStop = true;
+            this.rdmanager.Text = "MANAGER";
+            this.rdmanager.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(172, 228);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 34);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtuser
+            // 
+            this.txtuser.Font = new System.Drawing.Font("Monotype Corsiva", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtuser.Location = new System.Drawing.Point(201, 75);
+            this.txtuser.Name = "txtuser";
+            this.txtuser.Size = new System.Drawing.Size(175, 31);
+            this.txtuser.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(75, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 22);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Login";
+            // 
+            // Login
+            // 
+            this.Login.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Login.Location = new System.Drawing.Point(184, 170);
+            this.Login.Name = "Login";
+            this.Login.Size = new System.Drawing.Size(90, 32);
+            this.Login.TabIndex = 17;
+            this.Login.Text = "Login";
+            this.Login.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(75, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 22);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Password";
+            // 
+            // txtpassword
+            // 
+            this.txtpassword.Font = new System.Drawing.Font("Monotype Corsiva", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtpassword.Location = new System.Drawing.Point(201, 120);
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.PasswordChar = '*';
+            this.txtpassword.Size = new System.Drawing.Size(175, 31);
+            this.txtpassword.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.txtpassword);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtuser);
+            this.groupBox1.Controls.Add(this.Login);
+            this.groupBox1.Font = new System.Drawing.Font("Monotype Corsiva", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.groupBox1.Location = new System.Drawing.Point(12, 148);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(484, 333);
+            this.groupBox1.TabIndex = 23;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "LOGIN";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // login1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::automobile.Properties.Resources.CHS_background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(997, 588);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "login1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "login1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.login1_Load);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton rduser;
+        private System.Windows.Forms.RadioButton rdadmin;
+        private System.Windows.Forms.RadioButton rdmanager;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtuser;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Login;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtpassword;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
